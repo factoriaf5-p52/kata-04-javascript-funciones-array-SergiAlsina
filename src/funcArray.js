757a6e47c2a028fe2cb8096db93d9705f0537eaf
Sergi <sergialsina81@gmail.com>
2022-11-21 21:30:25 -0100

    exercici1

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,16 +1,35 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(){}
-export function findLongestWord(){}
-export function sumArray(){}
+export function maxOfTwoNumbers(a, b){
+    if (a>b){
+        return a 
+    } else  {
+        return b
+    }
+    }
+
+// ejercicio2
+
+let words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+export function findLongestWord(words){
+    if(!words.length) {
+        return undefined;}
+        
+}
+
+
+//ejercicio3
+
+let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+
+
+export function sumArray(numbers){}
+
 export function averageNumbers(){}
 export function averageWordLength(){}
 export function uniquifyArray(){}
 export function doesWordExist(){}
 export function howManyTimes(){}
 export function greatestProduct(){}
-
-
-
-
-
